f5894c2c1411574b49a1179ab180ca575f365260
MatResh <127337583+MatResh@users.noreply.github.com>
2025-05-10 16:24:45 +0300

    Update learn01.py

--- learn01.py
+++ learn01.py
@@ -1,1 +1,24 @@
-print ("hello world!")
+import numpy as np
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/linreg', methods=['POST'])
+def linreg():
+    data = request.json
+    xdata = data.get('xdata')
+    ydata = data.get('ydata')
+    x = data.get('x')
+    xn = np.array(x)
+
+    if xdata is None or ydata is None or x is None or len(xdata) != len(ydata):
+        return jsonify({'error': 'Invalid input'}), 400
+
+    m, b = np.polyfit(xdata, ydata, 1)
+
+    y = m * x + b
+
+    return jsonify({'y': y.tolist()})  
+
+if __name__ == '__main__':
+    app.run(debug=True)
